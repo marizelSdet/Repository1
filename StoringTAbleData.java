481bbedcfc989c6bbfc8d74a6c80a68f7858b8f7
marizelSdet <135082402+marizelSdet@users.noreply.github.com>
2023-05-30 22:04:52 -0400

    Create StoringTAbleData.java

---
+++ StoringTAbleData.java
@@ -0,0 +1,31 @@
+package day21_2DArray;
+
+public class StoringTAbleData {
+
+	public static void main(String[] args) {
+		
+		 String companies[] = {"Alfreds Futterkiste","Centro comercial Moctezuma","Ernst Handel","Island Trading","Laughing Bacchus Winecellars","Magazzini Alimentari Riuniti"};
+	        String contactes[] = {"Maria Anders","Francisco Chang","Roland Mendel","Helen Bennett","Yoshi Tannamuri","Giovanni Rovelli"    };
+	        String contries[] = {"Germany","Mexico","Austria","UK","Canada","Italy"};
+	    
+	        String table[][] = {companies,contactes , contries }; // to access all array elements
+	        
+	        
+//	        for (int i = 0 ; i < table.length ; i++) {
+//	            
+//	            for (int j = 0 ; j < table[i].length ; j++) {
+//	                System.out.println(table[i][j]);
+//	            }
+//	            
+//	        }
+	        
+	        
+	        for (int i = 0 ; i < companies.length ; i++) {
+	            System.out.println(companies[i] +"\t"+contactes[i]+"\t"+contries[i]); 
+	            // will print in a colum and row format
+	        }
+	        
+
+	}
+
+}
